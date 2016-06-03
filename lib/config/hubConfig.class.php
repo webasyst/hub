8cd84376b5f9f90f8e6741f9358f44718d85b710
Eugen <jk@47.kiev.ua>
2016-06-03 18:17:27 +0300

    Роутинг для плагинов

--- lib/config/hubConfig.class.php
+++ lib/config/hubConfig.class.php
@@ -3,6 +3,8 @@
 class hubConfig extends waAppConfig
 {
 
+    protected $_routes;
+    
     public function onInit()
     {
         $wa = wa();
@@ -11,6 +13,41 @@ class hubConfig extends waAppConfig
             $this->setCount($this->onCount());
             $this->maybeArchiveTopics();
         }
+    }
+    
+    public function getRouting($route = array(), $dispatch = true)
+    {
+        if ($this->_routes === null || $dispatch) {
+            $routes = parent::getRouting($route);
+            /**
+             * Extend routing via plugin routes
+             * @event routing
+             * @param array $routes
+             * @return array routes collected for every plugin
+             */
+            $result = wa()->event(array($this->application, 'routing'), $routes);
+            $all_plugins_routes = array();
+            foreach ($result as $plugin_id => $routing_rules) {
+                if ($routing_rules) {
+                    $plugin = str_replace('-plugin', '', $plugin_id);
+                    foreach ($routing_rules as $url => &$route) {
+                        if (!is_array($route)) {
+                            list($route_ar['module'], $route_ar['action']) = explode('/', $route);
+                            $route = $route_ar;
+                        }
+                        $route['plugin'] = $plugin;
+                        $all_plugins_routes[$url] = $route;
+                    }
+                    unset($route);
+                }
+            }
+            $routes = array_merge($all_plugins_routes, $routes);
+            if ($dispatch) {
+                return $routes;
+            }
+            $this->_routes = $routes;
+        }
+        return $this->_routes;
     }
 
     /**
